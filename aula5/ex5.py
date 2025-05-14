b7ad226badb1c16b5c698d3a016d45fcf4af1777
Erick Silvestre <109907858+Trombada@users.noreply.github.com>
2025-05-14 14:15:58 -0300

    Update ex5.py

--- aula5/ex5.py
+++ aula5/ex5.py
@@ -3,12 +3,34 @@ corredor1 = corredor2 = corredor3 = 0
 primeiro = segundo = terceiro = ""
 
 for i in range(11):
+    if corredor2 < 10:
+        if corredor2 == 0:
+            corredor2 += 1
+            print(f"Corredor 1 avançou {corredor1}")
+        else:
+            corredor2 += random.randint(1, 3)
+            if corredor2 > 10:
+                corredor2 = 10
+            print(f"Corredor 2 avançou {corredor2}")
     if corredor1 < 10:
         if corredor1 == 0:
             corredor1 += 1
             print(f"Corredor 1 avançou {corredor1}")
-        else: 
+        else:
+            corredor2 += random.randint(1, 3)
+            if corredor2 > 10:
+                corredor2 = 10
+            print(f"Corredor 2 avançou {corredor2}")
+    if corredor3 < 10:
+        if corredor3 == 0:
+            corredor3 += 1
+            print(f"Corredor 3 avançou {corredor3}")
+        else:
             corredor1 += random.randint(1, 3)
-            if corredor1 > 10:
-                corredor1 = 10
-            print(f"Corredor 1 vançou {corredor1}")
+            if corredor3 > 10:
+                corredor3 = 10
+            print(f"Corredor 3 avançou {corredor3}")
+
+
+
+  
