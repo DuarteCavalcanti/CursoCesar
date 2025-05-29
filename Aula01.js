135b51ad4370f3b18f9fc8726dd5057b4f42b603
Erick Silvestre <109907858+Trombada@users.noreply.github.com>
2025-05-29 18:47:28 -0300

    Create Aula01.js

---
+++ Aula01.js
@@ -0,0 +1,10 @@
+let clima = prompt("Digite como está o clima:");
+if (clima === "ensolarado") {
+   alert("O sol brilha, sua energia também!");
+} else if (clima === "chuvoso") {
+   alert("A chuva cai, mas você sempre pode voar!");
+} else if (clima === "nublado") {
+    alert("O céu pode estar cinza, mas seu dia pode ser colorido!");
+} else  {
+    alert(" Não compreendi!");
+}
