55208757865d64caed0a534256a1c551bb519418
umjotta <heliojuniorsly@gmail.com>
2025-06-06 19:28:15 -0300

    Add files via upload

---
+++ breninho/Flask2/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+prod = []
+
+@app.route('/', methods=['GET', 'POST'])
+
+def gerenciar_produtos():
+    if request.method == 'POST':  # Apenas adiciona quando for POST
+        nome_prod = request.form.get('nome_prod')
+        preco = request.form.get('preco')
+        desc = request.form.get('desc')
+
+        if nome_prod and preco:  # Verifica se os campos obrigatórios foram preenchidos
+            prod.append({'Nome': nome_prod, 'preco': preco, 'desc': desc})
+
+    return render_template('prod.html', prod=prod)  # Renderiza a lista atualizada
+
+if __name__ == '__main__':
+    app.run(debug=True)
